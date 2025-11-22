81f6f5ba644f5565a3b6a8bd1600421a7a1bbfa1
seatosky2002 <seatosky2002@gmail.com>
2025-11-22 10:32:32 +0900

    아이폰 10~15 추가

--- src/utils/apiMapper.js
+++ src/utils/apiMapper.js
@@ -14,13 +14,27 @@ const modelKoreanMap = {
   'iPhone 16': '아이폰 16',
   'iPhone 16 Pro': '아이폰 16 프로',
   'iPhone 16 Pro Max': '아이폰 16 프로 맥스',
+  'iPhone 16 +': '아이폰 16 플러스',
+  'iPhone 16e': '아이폰 16e',
   'iPhone 15': '아이폰 15',
   'iPhone 15 Pro': '아이폰 15 프로',
   'iPhone 15 Pro Max': '아이폰 15 프로 맥스',
+  'iPhone 15 +': '아이폰 15 플러스',
   'iPhone 14': '아이폰 14',
   'iPhone 14 Pro': '아이폰 14 프로',
   'iPhone 14 Pro Max': '아이폰 14 프로 맥스',
-  // 필요하면 추가
+  'iPhone 14+': '아이폰 14 플러스',
+  'iPhone 13': '아이폰 13',
+  'iPhone 13 Pro': '아이폰 13 프로',
+  'iPhone 13 Pro Max': '아이폰 13 프로 맥스',
+  'iPhone 13 Mini': '아이폰 13 미니',
+  'iPhone 12': '아이폰 12',
+  'iPhone 12 Pro': '아이폰 12 프로',
+  'iPhone 12 Pro Max': '아이폰 12 프로 맥스',
+  'iPhone 12 Mini': '아이폰 12 미니',
+  'iPhone 11': '아이폰 11',
+  'iPhone 11 Pro': '아이폰 11 프로',
+  'iPhone 11 Pro Max': '아이폰 11 프로 맥스',
 };
 
 /**
