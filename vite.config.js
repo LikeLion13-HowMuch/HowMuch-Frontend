474a71634e60ceed1f45bcece90c30ab989a801a
seatosky2002 <seatosky2002@gmail.com>
2025-12-20 00:12:05 +0900

    Modified vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,9 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    host: true, // 외부(Nginx)에서 접속할 수 있도록 허용
+    port: 5173, // 현재 사용 중인 포트
+    allowedHosts: ['howmuchapple.store'] // 도메인 차단 해제
+  }
 });
